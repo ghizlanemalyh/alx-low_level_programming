b35da8270c77d310011bbb13c1ee62d2ed6266d0
ghizlanemalyh <ghizlanemalyh0210@gmail.com>
2023-09-08 16:29:33 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,12 +9,12 @@
 
 int main(void)
 {
-	int n = 97;
-	while (n <= 122)
-	{
-		putchar(n);
-		n++;
-	}
-	putchar ('\n');
-	return (0);
+int n = 97;
+while (n <= 122)
+{
+putchar(n);
+n++;
+}
+putchar ('\n');
+return (0);
 }
