c5ee7a8dbc4e6ec373ddec8a3e088c09473f3791
ghizlanemalyh <ghizlanemalyh0210@gmail.com>
2023-09-08 13:33:18 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,11 +12,8 @@ int main(void)
         int n;
 
         srand(time(0)); /* Seed the random number generator */
-
         n = rand() - RAND_MAX / 2; /* Generate a random number */
-
         printf("%d is ", n); /* Print the random number */
-
         if (n > 0)
         {
                 printf("positive\n");
