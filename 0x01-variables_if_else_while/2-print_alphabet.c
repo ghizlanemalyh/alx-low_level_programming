b297ab9da0fe103418130142b32a936aaf218fa2
ghizlanemalyh <ghizlanemalyh0210@gmail.com>
2023-09-08 16:50:24 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,17 +4,17 @@
  *
  * Discription: 'prints the alphabet in lowercase'
  *
- * Return; Always 0
+ * Return: Always 0
  */
 
 int main(void)
 {
-int n = 97:
+int n = 97;
 while (n <= 122)
 {
-putchar(n):
-n++:
+putchar(n);
+n++;
 }
-putchar ('\n'):
-return (0):
+putchar ('\n');
+return (0);
 }
