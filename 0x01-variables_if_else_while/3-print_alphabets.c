550cafcbe624d7d1aba2455977dd877daf96f584
ghizlanemalyh <ghizlanemalyh0210@gmail.com>
2023-09-08 17:08:53 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,20 +6,20 @@
 
 int main(void)
 {
-	char c;
-	char d;
-	c = 'a';
-	d = 'A'
-	while
-		(c <= 'z') {
-			putchar(c);
-				c++;
-		}
-	while
-		(d <= 'Z') {
-			putchar (d);
-			d++;
-		}
-	putchar('\n');
-	return (0);
+char c;
+char d;
+c = 'a';
+d = 'A'
+while
+(c <= 'z') {
+putchar(c);
+c++;
+}
+while
+(d <= 'Z') {
+putchar (d);
+d++;
+}
+putchar('\n');
+return (0);
 }
