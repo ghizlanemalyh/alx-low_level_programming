da717b96986eb4d39519bb331a4e90c4796495a2
ghizlanemalyh <ghizlanemalyh0210@gmail.com>
2023-09-09 12:03:14 -0700

    print_alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,16 +8,16 @@ int main(void)
 {
 int n = 97;
 int m = 65;
-while(n <= 122)
+while (n <= 122)
 {
-putchar(n);
+putchar (n);
 n++;
 }
-while(m <= 90)
+while (m <= 90)
 {
-putchar(m);
+putchar (m);
 m++;
 }
-putchar('\n');
-return(0);
+putchar ('\n');
+return (0);
 }
