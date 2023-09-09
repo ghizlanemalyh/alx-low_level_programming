5ce3a5e8e5e96560865028fa846bb270c9ca3a3d
ghizlanemalyh <ghizlanemalyh0210@gmail.com>
2023-09-09 11:17:00 -0700

    print_alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,9 +9,9 @@ int main(void)
 {
 char alp[52] = "abcdefghijklmnopqrstuvwxyz";
 int i;
-for (i = 0; i < 52; i++)
+for (i = 0; i < 52; i++);
 {
-putchar(alp[i])
+putchar(alp[i]);
 }
 putchar('\n');
 return (0);
