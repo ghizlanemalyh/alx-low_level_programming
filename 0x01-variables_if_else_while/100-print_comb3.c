841fdc2e6282d9afc3d8f0397a8d6a022147d586
ghizlanemalyh <ghizlanemalyh0210@gmail.com>
2023-09-10 07:59:30 -0700

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,27 +6,26 @@
  */
 int main(void)
 {
-int digit1 = 0, digit2;
-while (digit1 <= 9)
-{
-digit2 = 0;
-while (digit2 <= 9)
-{
-if (digit1 != digit2 && digit1 < digit2)
-{
-putchar(digit1 + 48);
-putchar(digit2 + 48);
 
-if (digit1 + digit2 != 17)
-{
-putchar(',');
-putchar(' ');
-}
-digit2++;
-}
-digit1++;
-}
-}
-putchar('\n');
-return (0);
+        int d, p;
+
+        for (d = '0'; d < '9'; d++)
+        {
+
+        for (p = d + 1; p <= '9'; p++)
+        {
+
+        if (p != d)
+        {
+        putchar(d);
+        putchar(p);
+        if (d == '8' && p == '9')
+        continue;
+        putchar(',');
+        putchar(' ');
+        }
+        }
+        }
+        putchar('\n');
+        return (0);
 }
