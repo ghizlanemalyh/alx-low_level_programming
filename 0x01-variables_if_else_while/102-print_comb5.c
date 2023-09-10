8d2dfcc19f41fe275de9b5a12ec8c1d702490e25
ghizlanemalyh <ghizlanemalyh0210@gmail.com>
2023-09-10 10:43:57 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,35 +1,31 @@
 #include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
+ 
 int main(void)
 {
-int Digit1 = 0, Digit2;
-while (Digit1 <= 99)
+int Digit2, Digit1;
+
+while (Digit1 < 100)
+         {
+Digit1 = 0;
+while (Digit2 < 100)
 {
-Digit2 = Digit1;
-while (Digit2 <= 99)
-{
-if (Digit2 < Digit1)
+Digit2 = 0;
+if (Digit1 < Digit2)
 {
 putchar((Digit1 / 10) + 48);
 putchar((Digit1 % 10) + 48);
 putchar(' ');
 putchar((Digit2 / 10) + 48);
 putchar((Digit2 % 10) + 48);
-{
 if (Digit1 != 98 || Digit2 != 99)
 {
 putchar(',');
 putchar(' ');
 }
+}
 Digit2++;
+}
 Digit1++;
-}
-}
-}
 }
 putchar('\n');
 return (0);
