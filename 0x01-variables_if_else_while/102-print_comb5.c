357b05f3575cd5b662eb2c0a6a8a805b44cd24d7
ghizlanemalyh <ghizlanemalyh0210@gmail.com>
2023-09-10 09:04:43 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int Digit1 = 0, Digit2;
+
+while (Digit1 <= 99)
+{
+Digit2 = Digit1;
+while (Digit2 <= 99)
+{
+if (Digit2 != Digit1)
+putchar((Digit1 / 10) + 48);
+putchar((Digit1 % 10) + 48);
+putchar(' ');
+putchar((Digit2 / 10) + 48);
+putchar((Digit2 % 10) + 48);
+if (Digit1 != 98 || Digit2 != 99)
+{
+putchar(',');
+putchar(' ');
+}
+Digit2++;
+
+Digit1++;
+}
+}
+putchar('\n');
+return (0);
+}
