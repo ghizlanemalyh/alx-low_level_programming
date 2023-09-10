9b8c836433b632249095cf486e2dcbb926850326
ghizlanemalyh <ghizlanemalyh0210@gmail.com>
2023-09-09 18:58:54 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,8 +4,7 @@
  *Return: always 0
  */
 int main(void)
-
-{	
+{
 int n;
 for (n = 48; n <= 57; n++)
 {
@@ -17,7 +16,6 @@ continue;
 putchar(',');
 putchar(' ');
 }
-
-putchar('\n'); 
+putchar('\n');
 return (0);
 }
