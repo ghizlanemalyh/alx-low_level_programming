6cd4e87c50c5c643e57037400c46175bac0c3ad7
ghizlanemalyh <ghizlanemalyh0210@gmail.com>
2023-09-12 15:32:50 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,35 +6,37 @@
  */
 void times_table(void)
 {
-int r, c, p, t, o;
+	int r, c, p, t, o;
 
-for (r = 0; r <= 9; r++)
-{
-for (c = 0; c <= 9; c++)
-{
-p = r * c;
-t = p / 10;
-o = p % 10;
-if (c == 0)
-{
-_putchar('0');
-}
-if (p < 10)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(o + '0');
-}
-else if
-{
 
-_putchar(',');
-_putchar(' ');
-_putchar(t + '0');
-_putchar(o + '0');
-}
-}
-_putchar('\n');
-}
+	for (r = 0; r <= 9; r++)
+	{
+	for (c = 0; c <= 9; c++)
+	{
+	p = r * c;
+	t = p / 10;
+	o = p % 10;
+	if (c == 0)
+	{
+	_putchar('0');
+	}
+	else if (p < 10)
+	{
+	_putchar(',');
+	_putchar(' ');
+	_putchar(' ');
+	_putchar(o + '0');
+	}
+	else
+	{
+
+
+		_putchar(',');
+		_putchar(' ');
+		_putchar(t + '0');
+		_putchar(o + '0');
+	}
+	}
+	_putchar('\n');
+	}
 }
