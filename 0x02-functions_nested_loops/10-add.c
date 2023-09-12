42b17454da8311987edfc4b3a6611b2e85674dfa
ghizlanemalyh <ghizlanemalyh0210@gmail.com>
2023-09-12 15:36:59 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/**
+ * add - Entry point
+ *
+ * @A: integer on
+ * @B: integer two
+ *
+ * Return:sum.
+ *
+ */
+
+
+int add(int A, int B)
+{
+	int sum;
+
+
+	sum = A + B;
+	return (sum);
+}
