3d530dc32c1e148b6e99642e8be5e36c35ef883b
ghizlanemalyh <ghizlanemalyh0210@gmail.com>
2023-09-12 14:12:39 -0700

    . 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,22 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * print_alphabet_x10 - Make alphabet x10 times
- *
- * Return: void
+ * print_alphabet_x10 - print the alphabest 10 times
+ * Return: void here
  */
+
 void print_alphabet_x10(void)
 {
-	char c;
-	int i = 0;
+char c;
+int i = 0;
 
-	while (i = 9)
-	{
-		for (c = 'a'; c <= 'z'; c++)
-	}
-	putcha(c)
-
-	putchar('\n')
-	i++
+while (i <= 9)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+i++;
+}
 }
