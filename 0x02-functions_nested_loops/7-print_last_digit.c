02a0acbc73ce2e8aaaac9545be8747c8c95ea64e
ghizlanemalyh <ghizlanemalyh0210@gmail.com>
2023-09-12 14:46:51 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,25 +2,25 @@
 
 
 /**
- * print_d - entry point
+ * print_last_digit - entry point
  *
  *
- * @a:number to compute d
+ * @a:number to compute last digit
  *
- * Return: d
+ * Return: last digit
  */
 
 
-int print_d(int a)
+int print_last_digit(int a)
 {
-int d;
+	int last_digit;
 
 
-d = a % 10;
-if (d < 0)
-{
-d = d * -1;
-}
-_putchar(d + '0');
-return (d);
+	last_digit = a % 10;
+	if (last_digit < 0)
+	{
+		last_digit = last_digit * -1;
+	}
+	_putchar(last_digit + '0');
+	return (last_digit);
 }
