7ef2ef61fa8f1378bce422c515dc40375ca76808
ghizlanemalyh <ghizlanemalyh0210@gmail.com>
2023-09-13 11:33:22 -0700

    updated

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,11 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio>
+#include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_numbe(int a, int b, int c);
-int convert_day(int month, int day);
-void print_remaining_day(int month, int day, int year);
 
 #endif /* MAIN_H */
