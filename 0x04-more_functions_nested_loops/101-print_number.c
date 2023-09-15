c302d098763f7bebdd2fad672341592d702179c2
ghizlanemalyh <ghizlanemalyh0210@gmail.com>
2023-09-15 14:23:15 -0700

    updated

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_number - check the function code
+*
+* @n: variables
+*
+* Return: Always
+*/
+
+void print_number(int n)
+
+{
+unsigned int n1;
+if (n < 0)
+{
+n1 = -n;
+_putchar('-');
+}
+else
+{n1 = n;
+}
+if (n1 / 10)
+{
+print_number(n1 / 10);
+}
+_putchar((n1 % 10) + '0');
+}
