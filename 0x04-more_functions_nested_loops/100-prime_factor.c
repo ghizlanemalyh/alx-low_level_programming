858f8e73771036198421b12c78ffab84b0c339bf
ghizlanemalyh <ghizlanemalyh0210@gmail.com>
2023-09-15 14:17:26 -0700

    updated

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,4 +1,4 @@
- #include <stdio.h>
+#include <stdio.h>
 
 /**
 * main - Entery Point
@@ -8,26 +8,17 @@
 
 int main(void)
 {
-long int n;
-long int max;
-long int i;
-n = 612852475143;
-max = -1;
-while (n % 2 == 0)
-{
-max = 2;
-n /= 2;
-}
-for (i = 3; i <= sqrt(n); i = i + 2)
-{
-while (n % i == 0)
-{
-max = i;
-n = n / i;
-}
-}
-if (n > 2)
-max = n;
-printf("%ld\n", max);
-return (0);
+	long int i = 612852475143;
+	long int factors;
+
+	for (factors = 2; factors < i; factors++)
+	{
+		if (i % factors == 0)
+		{
+			i /= factors;
+		}
+	}
+
+	printf("%ld\n", factors);
+	return (0);
 }
