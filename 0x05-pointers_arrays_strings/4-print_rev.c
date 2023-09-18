92e6e1f005f69ff8dcd9bbb7f03d9ac95f9a7e08
ghizlanemalyh <ghizlanemalyh0210@gmail.com>
2023-09-18 16:56:49 -0700

    print_re.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,19 +6,20 @@
  */
 void print_rev(char *s)
 {
-int longi = 0;
-int o;
+	int longi = 0;
+	int o;
 
-while (*s != '\0')
-{
-longi++;
-s++;
-}
-s--;
-for (o -longi; o > 0; o--)
-{
-_putchar(*s);
-s--;
-}
-_putchar('\n')
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	s--;
+	for (o = longi; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
 }
