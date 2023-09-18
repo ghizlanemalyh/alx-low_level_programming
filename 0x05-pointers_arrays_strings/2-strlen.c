3be5d66492dc28b3f2e7ea93a63169374c6e2dd0
ghizlanemalyh <ghizlanemalyh0210@gmail.com>
2023-09-18 16:16:24 -0700

    updadted

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _strlen - return the length of a string
- *  @s: string
- *  Return: length
+ *_strlen - return the length of a string
+ *@s: string
+ *Return: length
  */
 int _strlen(char *s)
 {
