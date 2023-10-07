0eccf56cbc02cea3cf0492f8db794ec90d0c91e8
ghizlanemalyh <ghizlanemalyh0210@gmail.com>
2023-10-07 21:24:54 +0100

    updated

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,9 +7,7 @@
 *
 * Return: the int converted from the string
 */
-
 int _atoi(char *s)
-
 {
 		int i, d, n, len, f, digit;
 
@@ -25,28 +23,28 @@ int _atoi(char *s)
 
 		while (i < len && f == 0)
 		{
+
 			if (s[i] == '-')
 			++d;
 
 			if (s[i] >= '0' && s[i] <= '9')
 			{
-																									digit = s[i] - '0';
-																									if (d % 2)
-																									digit = -digit;
-																									n = n * 10 + digit;
-																									f = 1;
-																									if (s[i + 1] < '0' || s[i + 1] > '9')
-																									break;
-																									f = 0;
-																								}
+				digit = s[i] - '0';
+				if (d % 2)
+				digit = -digit;
+				n = n * 10 + digit;
+				f = 1;
+				if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+				f = 0;
+			}
 			i++;
-		}
+													}
 
-		if (f == 0)
+													if (f == 0)
+													return (0);
 
-		return (0);
-
-		return (n);
+													return (n);
 }
 
 /**
@@ -56,9 +54,7 @@ int _atoi(char *s)
 *
 * Return: 0 (Success), 1 (Error)
 */
-
 int main(int argc, char *argv[])
-
 {
 		int result, num1, num2;
 
