130da1bd47e97bf53b2eb3a2b042202a3800ba5b
ghizlanemalyh <ghizlanemalyh0210@gmai.com>
2023-10-12 22:22:43 +0100

    updated

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- *  * struct dog - The Dog main struct.
- *   * @name: The dog name goes here.
- *    * @age: The age of that dog.
- *     * @owner: Who the owner? of the dog.
- *      */
+* struct dog - The Dog main struct.
+* @name: The dog name goes here.
+* @age: The age of that dog.
+* @owner: Who the owner? of the dog.
+*/
 typedef struct dog
 {
 		char *name;
 		float age;
 		char *owner;
-}dog;
+}
+dog;
 
 typedef dog dog_t;
 
