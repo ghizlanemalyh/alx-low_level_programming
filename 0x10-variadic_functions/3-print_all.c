89a88ef69bb128235d054535f80c4b8d45372753
ghizlanemalyh <ghizlanemalyh0210@gmai.com>
2023-10-14 16:29:12 +0100

    updated

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,48 +1,53 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
 
 /**
-* print_all - prints anything
-* @format: list of types of arguments passed to the function
+* print_all - a function that prints anything.
+* @format: a list of types of arguments passed to the function.
+* Return: Just Nothing.
 */
+
 void print_all(const char * const format, ...)
 {
-		int i = 0;
-		char *str, *sep = "";
+		int indx, findic;
+		char *str;
+		va_list myapx;
 
-		va_list list;
+		indx = 0;
 
-		va_start(list, format);
-
-		if (format)
+		va_start(myapx, format);
+		while (format != NULL && format[indx] != '\0')
 		{
-			while (format[i])
+			switch (format[indx])
 			{
-				switch (format[i])
-				{
-					case 'c':
-						printf("%s%c", sep, va_arg(list, int));
-						break;
-																										case 'i':
-						printf("%s%d", sep, va_arg(list, int));
-						break;
-					case 'f':
-						printf("%s%f", sep, va_arg(list, double));
-					case 's':
-						str = va_arg(list, char *);
-						if (!str)
-						str = "(nil)";
-						printf("%s%s", sep, str);
-						break;
-					default:
-						i++;
-						continue;
+				case 'i':
+					printf("%i", va_arg(myapx, int));
+					findic = 0;
+					break;
+				case 's':
+					str = va_arg(myapx, char*);
+					if (str == NULL)
+					str = "(nil)";
+					printf("%s", str);
+					findic = 0;
+					break;
+				case 'c':
+					printf("%c", va_arg(myapx, int));
+					findic = 0;
+					break;
+				case 'f':
+					printf("%f", va_arg(myapx, double));
+					findic = 0;
+					break;
+				default:
+					findic = 1;
+					break;
 				}
-				sep = ", ";
-				i++;
+				if (findic == 0 && format[indx + 1] != '\0')
+				printf(", ");
+				indx++;
 			}
-		}
-		printf("\n");
-		va_end(list);
+			printf("\n");
+			va_end(myapx);
 }
